cd1f448d251efdb55791c3307ba812e9847b2a26
Joseph Rafferty <jrafferty@tamu.edu>
2012-06-04 13:55:59 -0500

    changing hostname to a bogus value initially working

--- untitled.rb
+++ untitled.rb
@@ -12,33 +12,30 @@ HOSTS = [
   "timmy.continuum.tamu.edu"
   ]
 
+# Fake host to point the port to
+BOGUS_HOST = "bogus.continuum.tamu.edu"
+
 # These are the lpr queues on each host we should check for
 QUEUES = [ "scc", "large" ]
 
 
-# This is a map of information about the printer's lpr queues
+# This is a map of information about the server's child printer ports
 #  Expected keys are:
 #    "load_balance_port" => the name of the port on the print server that is configured as the load balance port
 #    "print_queues" => name of the child print queues that should be in the load balance port, in the format "hostname" => "queue name"
 
-QUEUE_INFO = {
+PORTS = {
   "scc" => {
-    "load_balance_port" => "PCOUNT_TEST-SCC-SCC-LoadBalance",
-    "print_queues" => {
-      "cartman.continuum.tamu.edu"  => "TEST-SCC-cartman.continuum.tamu.edu",
-      "kyle.continuum.tamu.edu"     => "TEST-SCC-kyle.continuum.tamu.edu",
-      "stan.continuum.tamu.edu"     => "TEST-SCC-stan.continuum.tamu.edu",
-      "timmy.continuum.tamu.edu"    => "TEST-SCC-timmy.continuum.tamu.edu"
-    }
+    "cartman.continuum.tamu.edu"  => "PCOUNT_SCC-SCC-cartman.continuum.tamu.edu",
+    "kyle.continuum.tamu.edu"     => "PCOUNT_SCC-SCC-kyle.continuum.tamu.edu",
+    "stan.continuum.tamu.edu"     => "PCOUNT_SCC-SCC-stan.continuum.tamu.edu",
+    "timmy.continuum.tamu.edu"    => "PCOUNT_SCC-SCC-timmy.continuum.tamu.edu"
   },
   "large" => {
-    "load_balance_port" => "PCOUNT_TEST-SCC-LARGE-LoadBalance",
-    "print_queues" => {
-      "cartman.continuum.tamu.edu"  => "TEST-SCC-LARGE-cartman.continuum.tamu.edu",
-      "kyle.continuum.tamu.edu"     => "TEST-SCC-LARGE-kyle.continuum.tamu.edu",
-      "stan.continuum.tamu.edu"     => "TEST-SCC-LARGE-stan.continuum.tamu.edu",
-      "timmy.continuum.tamu.edu"    => "TEST-SCC-LARGE-timmy.continuum.tamu.edu"
-    }
+    "cartman.continuum.tamu.edu"  => "PCOUNT_SCC-LARGE-cartman.continuum.tamu.edu",
+    "kyle.continuum.tamu.edu"     => "PCOUNT_SCC-LARGE-kyle.continuum.tamu.edu",
+    "stan.continuum.tamu.edu"     => "PCOUNT_SCC-LARGE-stan.continuum.tamu.edu",
+    "timmy.continuum.tamu.edu"    => "PCOUNT_SCC-LARGE-timmy.continuum.tamu.edu"
   }
 }
 
@@ -55,42 +52,46 @@ TABLE_FORMAT = [
 
 # Method to add or remove entries in the registry for a given load balance port
 # params:
-# => action: add or delete
+# => action: on or off
 # => host:   the host to remove - will be used to lookup the host's queue name
 # => queue:  the printer lpr queue to modify the load balance port for
 
 def mod_reg(action, host, queue)
+  begin
+    key_name = PORTS[queue][host]
+  rescue Exception => e
+    puts "Something wrong with your hash, yo"
+    raise e
+  end
   
-  action = "push" if action == :add
-  action = "delete" if action == :delete
-  
-  queue_info =  QUEUE_INFO[queue]
-  
-  raise "No Load Balance Port name defined!" unless key_name = queue_info["load_balance_port"]
   
   key_path = "SYSTEM\\CurrentControlSet\\Control\\Print\\Monitors\\Pcounter Port\\Ports\\#{key_name}"
   
   access = Win32::Registry::KEY_ALL_ACCESS
   begin
     Win32::Registry::HKEY_LOCAL_MACHINE.open(key_path, access) do |reg|
-      current_printers = reg["LoadBalancePrinters"]
-      raise "No child queues defined for #{queue}" unless queues = queue_info["print_queues"]
-      raise "No queue name defined for host: #{host}" unless queues[host]
-      current_printers.send(action, queues[host])
-      current_printers.uniq!
-    	reg["LoadBalancePrinters"] = current_printers
+      current_hostname = reg["HostName"]
+      begin
+        if current_hostname == BOGUS_HOST && action == :on
+          reg["HostName"] = host
+        elsif current_hostname == host && action == :off
+          reg["HostName"] = BOGUS_HOST
+        end
+      rescue Win32::Registry::Error => e
+        puts "Error writing to the registry key: #{key_path}"
+      end
     end
   rescue Win32::Registry::Error => e
     puts "Error reading the registry key #{key_path}: #{e.message}"
   end
 end
 
-def remove_host_from_loadbalance(host, queue)
-  mod_reg(:delete, host, queue)
+def disable_host_for_queue(host, queue)
+  mod_reg(:off, host, queue)
 end
 
-def add_host_to_loadbalance(host, queue)
-  mod_reg(:add, host, queue)
+def enable_host_for_queue(host, queue)
+  mod_reg(:on, host, queue)
 end
 
 
@@ -134,15 +135,15 @@ HOSTS.each do |host|
     QUEUES.each do |key|
       if queues[key]
         if queues[key]["accepts"] == "Yes" && queues[key]["releases"] == "Yes"
-          puts "Queue #{key} on #{host} is accepting and releasing, adding to load balance port"
-          add_host_to_loadbalance(host, key)
+          puts "Queue #{key} on #{host} is accepting and releasing, ensuring port hostname is accurate"
+          enable_host_for_queue(host, key)
         else
-          puts "Queue #{key} on #{host} is either not accepting or not releasing, removing from load balance port"
-          remove_host_from_loadbalance(host, key)
+          puts "Queue #{key} on #{host} is either not accepting or not releasing, changing port hostname to bogus"
+          disable_host_for_queue(host, key)
         end
       else
         puts "#{host} has no queue named #{key}! Removing from #{key} load balance port"
-        remove_host_from_loadbalance(host, key)
+        disable_host_for_queue(host, key)
       end
     end
   else
